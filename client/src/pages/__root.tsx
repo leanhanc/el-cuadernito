92f2486b612f32a8eb34e98a294c93191f5bf3e1
Leandro Hanc <leandrohanc@gmail.com>
2024-03-03 10:09:19 -0300

    Remove unused import

--- client/src/pages/__root.tsx
+++ client/src/pages/__root.tsx
@@ -1,7 +1,6 @@
 /* TanStack */
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
 import { QueryClient } from '@tanstack/react-query';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 /* Fonts */
